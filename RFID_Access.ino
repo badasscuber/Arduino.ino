4ec4ed4685eb72a71c9f8b0859121f698178ee5a
Srikanth Shankar <srikrishshankar180498@gmail.com>
2018-02-18 12:29:48 +0530

    Add files via upload

---
+++ RFID_Access.ino
@@ -0,0 +1,84 @@
+/*
+ *  Created by TheCircuit
+*/
+/* 
+ NODEMCU      MFRC522
+D7-           MOSI
+D6-           MOSI
+SD2-          RST
+SD3-         SDA/SS
+D8-           SCK
+*/
+constexpr uint8_t RST_PIN = 9;          // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10; 
+#include <SPI.h>
+#include <MFRC522.h>
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+int statuss = 0;
+int out = 0;
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.println();
+  Serial.print(" UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println();
+  if (content.substring(1) == "2B 6E 00 99") //change UID of the card that you want to give access
+  {
+    Serial.println(" Access Granted ");
+    Serial.println(" Welcome Mr.Sowmik ");
+    delay(1000);
+    Serial.println(" Have FUN ");
+    Serial.println();
+    statuss = 1;
+  }
+   else if (content.substring(1) == "A7 51 5D 59") //change UID of the card that you want to give access
+  {
+    Serial.println(" Access Granted ");
+    Serial.println(" Welcome Mr.Abhishek");
+    delay(1000);
+    Serial.println(" Have FUN ");
+    Serial.println();
+    statuss = 1;
+  }
+
+   else if (content.substring(1) == "0B F0 EE 66") //change UID of the card that you want to give access
+  {
+    Serial.println(" Access Granted ");
+    Serial.println(" Welcome Mr.Srikanth");
+    delay(1000);
+    Serial.println(" Have FUN ");
+    Serial.println();
+    statuss = 1;
+  }
+  else   {
+    Serial.println(" Access Denied ");
+    delay(3000);
+  }
+} 
